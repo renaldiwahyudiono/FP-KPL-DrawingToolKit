3680f233abab893c66324e5aafab4aa0e2928990
Renaldi Wahyudiono <32060371+renaldiwahyudiono22@users.noreply.github.com>
2018-12-21 21:39:42 +0700

    v2.4

---
+++ FP KPL/DiagramToolkit/MainWindow.cs
@@ -0,0 +1,82 @@
+﻿using DiagramToolkit.Commands;
+using DiagramToolkit.MenuItems;
+using DiagramToolkit.ToolbarItems;
+using DiagramToolkit.Tools;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace DiagramToolkit
+{
+    public partial class MainWindow : Form
+    {
+        private IToolbox toolbox;
+        private IEditor editor;
+        private IToolbar toolbar;
+        private IMenubar menubar;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitUI();
+        }
+
+        private void InitUI()
+        {
+            Debug.WriteLine("Initializing UI objects.");
+
+            #region Editor and Canvas
+
+            Debug.WriteLine("Loading canvas...");
+            this.editor = new DefaultEditor();
+            this.toolStripContainer1.ContentPanel.Controls.Add((Control)this.editor);
+
+            ICanvas canvas2 = new DefaultCanvas();
+            canvas2.Name = "Layer";
+            this.editor.AddCanvas(canvas2);
+
+            #endregion
+
+            #region Commands
+
+            //BlackCanvasBgCommand blackCanvasBgCmd = new BlackCanvasBgCommand(this.canvas);
+            //WhiteCanvasBgCommand whiteCanvasBgCmd = new WhiteCanvasBgCommand(this.canvas);
+
+            #endregion
+
+            #region Toolbox
+
+            // Initializing toolbox
+            Debug.WriteLine("Loading toolbox...");
+            this.toolbox = new DefaultToolbox();
+            this.toolStripContainer1.LeftToolStripPanel.Controls.Add((Control)this.toolbox);
+            this.editor.Toolbox = toolbox;
+
+            #endregion
+
+            #region Tools
+
+            // Initializing tools
+            Debug.WriteLine("Loading tools...");
+            this.toolbox.AddTool(new SelectionTool());
+            this.toolbox.AddSeparator();
+            this.toolbox.AddTool(new LineTool());
+            this.toolbox.AddTool(new RectangleTool());
+            this.toolbox.AddTool(new TextTool());
+            this.toolbox.ToolSelected += Toolbox_ToolSelected;
+
+            #endregion
+
+        }
+
+        private void Toolbox_ToolSelected(ITool tool)
+        {
+            if (this.editor != null)
+            {
+                Debug.WriteLine("Tool " + tool.Name + " is selected");
+                ICanvas canvas = this.editor.GetSelectedCanvas();
+                canvas.SetActiveTool(tool);
+                tool.TargetCanvas = canvas;
+            }
+        }
+    }
+}
---
+++ FP KPL/DiagramToolkit/Tools/RedFillColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramToolkit.Tools
+{
+    public partial class RedFillColor : Component
+    {
+        public RedFillColor()
+        {
+            InitializeComponent();
+        }
+
+        public RedFillColor(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
